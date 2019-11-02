87e3c747824d13a7430a9f858341927e612ff07c
adelpino <alejandro.ignacio.dpa@gmail.com>
2019-11-02 11:22:35 -0300

    Super Coordinador: Se corrige problemas de visualizacion de las solicitudes

--- app/Http/Controllers/SuperController.php
+++ app/Http/Controllers/SuperController.php
@@ -15,21 +15,21 @@ class SuperController extends Controller
    public function index()
     {
         $listaJustificacionesValidando = DB::table('justifications')
-            ->select('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
+            ->select('justifications.ID_DATO','NFOLIO', 'justifications.RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
             ->join('datos_semestre', 'justifications.correo_alum', 'datos_semestre.correo_alum')
             ->where([['estado', 'like', 'Pendiente']])
             ->groupBy('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
             ->get();
 
         $listaJustificacionesAprobadas = DB::table('justifications')
-            ->select('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
+            ->select('justifications.ID_DATO','NFOLIO', 'justifications.RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
             ->join('datos_semestre', 'justifications.correo_alum', 'datos_semestre.correo_alum')
             ->where([['estado', 'like', 'Aprobado']])
             ->groupBy('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
             ->get();
 
         $listaJustificacionesRechazadas = DB::table('justifications')
-            ->select('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
+            ->select('justifications.ID_DATO','NFOLIO', 'justifications.RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
             ->join('datos_semestre', 'justifications.correo_alum', 'datos_semestre.correo_alum')
             ->where([['estado', 'like', 'Rechazado']])
             ->groupBy('justifications.ID_DATO','NFOLIO', 'RUT_ALU', 'justifications.NOMBRE_ALUM', 'FEC_SOL', 'FEC_JUS', 'ASIGNATURA','ESTADO')
